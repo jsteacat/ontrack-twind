e1ee3a921dc29e0e07549ac2c148d769b9a5a1b4
Stepan Kormilin <ubscoder@gmail.com>
2024-02-05 11:30:47 +0300

    activity item component

---
+++ app/src/components/activities/ActivityItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+import BaseButton from '@/components/base/BaseButton.vue'
+import BaseSelect from '@/components/base/BaseSelect.vue'
+import { TrashIcon } from '@heroicons/vue/24/outline'
+
+defineProps(['activity'])
+
+const periodSelectOptions = [
+  {
+    value: 15,
+    label: '0:15'
+  },
+  {
+    value: 30,
+    label: '0:30'
+  },
+  {
+    value: 45,
+    label: '0:45'
+  }
+]
+
+const secondsToComplete = ref(null)
+</script>
+
+<template>
+  <li class="flex flex-col gap-2 p-4">
+    <div class="flex items-center gap-2">
+      <BaseButton>
+        <TrashIcon class="h-8" />
+      </BaseButton>
+      <span class="truncate text-xl">{{ activity }}</span>
+    </div>
+    <div>
+      <BaseSelect
+        placeholder="h:mm"
+        :options="periodSelectOptions"
+        :selected="secondsToComplete"
+        class="font-mono"
+        @select="secondsToComplete = $event"
+      />
+    </div>
+  </li>
+</template>
--- app/src/pages/TheActivities.vue
+++ app/src/pages/TheActivities.vue
@@ -1,22 +1,5 @@
 <script setup>
-import BaseButton from '@/components/base/BaseButton.vue'
-import BaseSelect from '@/components/base/BaseSelect.vue'
-import { TrashIcon } from '@heroicons/vue/24/outline'
-
-const periodSelectOptions = [
-  {
-    value: 15,
-    label: '0:15'
-  },
-  {
-    value: 30,
-    label: '0:30'
-  },
-  {
-    value: 45,
-    label: '0:45'
-  }
-]
+import ActivityItem from '@/components/activities/ActivityItem.vue'
 
 const activities = ['Coding', 'Reding', 'Training']
 </script>
@@ -24,17 +7,7 @@ const activities = ['Coding', 'Reding', 'Training']
 <template>
   <div>
     <ul class="divide-y">
-      <li v-for="activity in activities" :key="activity" class="flex flex-col gap-2 p-4">
-        <div class="flex items-center gap-2">
-          <BaseButton>
-            <TrashIcon class="h-8" />
-          </BaseButton>
-          <span class="truncate text-xl">Reading</span>
-        </div>
-        <div>
-          <BaseSelect placeholder="h:mm" :options="periodSelectOptions" class="font-mono" />
-        </div>
-      </li>
+      <ActivityItem v-for="activity in activities" :key="activity" :activity="activity" />
     </ul>
   </div>
 </template>
