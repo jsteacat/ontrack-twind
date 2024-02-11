3b183036549bc27a13ea8b48fc1a17a4dc9f05dd
Stepan Kormilin <ubscoder@gmail.com>
2024-02-11 16:42:03 +0300

    scroll top after create activity

--- app/src/components/activities/TheActivityForm.vue
+++ app/src/components/activities/TheActivityForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
 import BaseButton from '@/components/base/BaseButton.vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
 import { isActivityItemValid } from '@/validators'
@@ -10,9 +10,11 @@ const emit = defineEmits({
 
 const activity = ref('')
 
-function submit() {
+async function submit() {
   emit('submit', activity.value)
   activity.value = ''
+  await nextTick()
+  window.scrollTo(0, -document.body.scrollHeight)
 }
 </script>
 
